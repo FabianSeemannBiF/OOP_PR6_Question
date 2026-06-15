6e2bfc0b248c6984d7eb0d2babe4f3681726c7ba
FabianSeemannBiF <if25b223@technikum-wien.at>
2026-06-15 15:24:45 +0200

    updated Question

--- includes/Question.h
+++ includes/Question.h
@@ -1,16 +1,23 @@
 #ifndef QUESTION_H
 #define QUESTION_H
 #include <string>
+#include <vector>
+
+struct Answer {
+    std::string name;
+    bool isCorrect;
+};
 
 class Question {
 public:
     //Constructor and Deconstructor
-    Question(std::string name, int pointValue);
+    Question(const std::string &name, int pointValue);
     virtual ~Question() = default;
 
     //Functions
-    void print();
+    void print() const;
     virtual void markAnswer(int answerNumber) = 0;
+    void addAnswer(const std::string &answerName, bool isCorrect);
 
     //Getters
     virtual int getPoints() = 0;
@@ -18,6 +25,7 @@ public:
 private:
     std::string name;
     int pointValue;
+    std::vector<Answer> answers;
 };
 
 #endif // QUESTION_H
--- src/Question.cpp
+++ src/Question.cpp
@@ -1,11 +1,22 @@
 #include "Question.h"
 #include <iostream>
 
-Question::Question(std::string name, int pointValue){
+Question::Question(const std::string &name, const int pointValue){
     this->name = name;
     this->pointValue = pointValue;
 }
 
-void Question::print(){
+void Question::print() const {
     std::cout << name << std::endl;
+    for (int i = 0; i < this->answers.size(); i++) {
+        std::cout << " [" << (i + 1) << "]" << this->answers[i].name << std::endl;
+    }
+}
+
+void Question::addAnswer(const std::string &answerName, const bool isCorrect) {
+    //this->answers.push_back({answerName, isCorrect});
+    Answer answer;
+    answer.name = answerName;
+    answer.isCorrect = isCorrect;
+    this->answers.push_back(answer);
 }
